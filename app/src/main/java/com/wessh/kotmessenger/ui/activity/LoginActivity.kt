95463eabb628439c919cf7346b84f22aa3be9e47
Wessh Walker <wesshbr@gmail.com>
2019-06-20 01:16:46 -0300

    Tela de Cadastro
    -> Implementado a tela de cadastro
    ~ Falta implementar a foto de usuário

--- app/src/main/java/com/wessh/kotmessenger/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/wessh/kotmessenger/ui/activity/LoginActivity.kt
@@ -2,12 +2,16 @@ package com.wessh.kotmessenger.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.wessh.kotmessenger.R
+import kotlinx.android.synthetic.main.act_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_login)
+
+        
     }
 }
--- app/src/main/java/com/wessh/kotmessenger/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/wessh/kotmessenger/ui/activity/RegisterActivity.kt
@@ -2,12 +2,15 @@ package com.wessh.kotmessenger.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.wessh.kotmessenger.R
+import kotlinx.android.synthetic.main.act_login.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_register)
+
     }
 }
