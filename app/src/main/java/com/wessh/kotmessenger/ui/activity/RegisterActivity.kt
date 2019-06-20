e152f72a1d84e33e5c1dfc1c868c5fddc2d3bb6b
Wessh Walker <wesshbr@gmail.com>
2019-06-20 01:09:21 -0300

    Atualização
    -> Implementado a tela de cadastro
    -> Adicionado novas strings para a activity

---
+++ app/src/main/java/com/wessh/kotmessenger/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.wessh.kotmessenger.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wessh.kotmessenger.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_register)
+    }
+}
