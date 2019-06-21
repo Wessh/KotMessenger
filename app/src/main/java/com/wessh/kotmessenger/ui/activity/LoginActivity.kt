c219c15aac446d98c2dd0de44c74d8c287596640
Wessh Walker <wesshbr@gmail.com>
2019-06-20 22:17:26 -0300

    Atualização
    -> Implementado a função de cadastro.
    -> Implementado a função de login

--- app/src/main/java/com/wessh/kotmessenger/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/wessh/kotmessenger/ui/activity/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.wessh.kotmessenger.R
 import kotlinx.android.synthetic.main.act_login.*
 
@@ -13,9 +15,33 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_login)
 
+        btn_login.setOnClickListener {
+            singIn()
+        }
+
         txt_account.setOnClickListener {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun singIn(){
+        val email = log_email.text.toString()
+        val password = log_password.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"email e senha devem ser informados", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    Log.i("Teste :", "Usuario Logado ${it.result?.user?.uid}")
+                }
+            }.addOnFailureListener {
+                Log.e("Teste :", it.message,it)
+            }
+
     }
 }
--- app/src/main/java/com/wessh/kotmessenger/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/wessh/kotmessenger/ui/activity/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.wessh.kotmessenger.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.wessh.kotmessenger.R
 import kotlinx.android.synthetic.main.act_register.*
 
@@ -13,11 +16,47 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.act_register)
 
         btn_registrar.setOnClickListener {
-            val email = reg_email.text.toString()
-            val password = reg_password.text.toString()
-
-            Log.i("Teste - ","Email: $email | Password: $password")
+            createUser()
         }
 
+        btn_photo.setOnClickListener {
+            selectPhoto()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0) {
+            val mSelectedUri = data?.data
+            Log.i("Teste", mSelectedUri.toString())
+        }
+    }
+
+
+    private fun selectPhoto(){ //Reler paginas 147 e 148
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+    }
+
+    private fun createUser(){
+        val email = reg_email.text.toString()
+        val password = reg_password.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,
+                "email e senha devem ser informados",
+                Toast.LENGTH_LONG).show()
+            return
+        }
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+            if(it.isSuccessful){
+                Log.i("Teste", "UserId é ${it.result?.user?.uid}")
+            }
+        }.addOnFailureListener {
+                Log.e("Teste :", it.message, it)
+            }
     }
 }
